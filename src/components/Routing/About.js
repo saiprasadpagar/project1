99f322adad6b378ffb62dfa077a51001971cc84d
Saiprasad <saiprasadpagar@gmail.com>
2023-04-17 17:14:30 +0530

    2nd commit

--- src/components/Routing/About.js
+++ src/components/Routing/About.js
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 const About =()=>{
     return(<>
 <div style = {{ backgroundColor : "lightgreen" , height:"200px"}}>
-<h2>About Component</h2>
+<h2>About Component:</h2>
 <NavLink to="contacts">Contactss</NavLink>
 </div>
     </>
